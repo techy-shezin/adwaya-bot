b0d85bee4be88d3ea78d2d67bace53c4fad4dfad
techy-shezin <muhammadshazin035@gmail.com>
2026-06-19 09:16:13 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -81,7 +81,7 @@ export const botConfig = {
     deleteApprovedAfter: 30,
 
     // Role IDs allowed to manage applications.
-    managerRoles: [], // Will be populated from environment or database
+    managerRoles: [Mothalaali,Thoyilaali], // Will be populated from environment or database
   },
 
   // =========================
