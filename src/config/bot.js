84e73c85e592d9d0674c410eae883a77f6caf82a
techy-shezin <muhammadshazin035@gmail.com>
2026-06-19 07:34:40 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made for ikru",
+        name: "Made for ikru vava",
         // Activity type number (0 = Playing).
         type: 0,
       },
